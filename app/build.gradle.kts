56282913696a7f98613ecc566afe8b753c89a32f
gloryciel <tgvae1122@gmail.com>
2024-11-18 14:52:09 +0900

    delete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -15,6 +17,19 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val localProperties = Properties()
+        val localPropertiesFile = rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { inputStream ->
+                localProperties.load(inputStream)
+            }
+        }
+        buildConfigField("String", "NAVER_MAPS_CLIENT_ID", "\"${localProperties.getProperty("com.naver.maps.map.CLIENT_ID")}\"")
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 
     buildTypes {
@@ -34,15 +49,22 @@ android {
         jvmTarget = "1.8"
     }
     viewBinding.isEnabled = true
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
-
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.naver.maps:map-sdk:3.19.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
