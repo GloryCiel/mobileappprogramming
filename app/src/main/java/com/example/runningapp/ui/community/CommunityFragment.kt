29a3eddb73256653655c84b7f448c5f01cbe5a87
tldnr1 <tldnr1@knu.ac.kr>
2024-11-27 02:08:43 +0900

    community item updated. 태그 부분 width 조정 필요

--- app/src/main/java/com/example/runningapp/ui/community/CommunityFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/community/CommunityFragment.kt
@@ -37,7 +37,7 @@ class MyAdapter(val datas: MutableList<String>) :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as MyViewHolder).binding
-        binding.itemCommunity.text = datas[position]
+        //binding.itemCommunity.text = datas[position]
 
         // 아이템 클릭 리스너 추가
         holder.itemView.setOnClickListener {
@@ -49,48 +49,6 @@ class MyAdapter(val datas: MutableList<String>) :
         }
     }
 }
-
-class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
-    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        super.onDrawOver(c, parent, state)
-        val width = parent.width
-        val height = parent.height
-
-        val dr: Drawable? = ResourcesCompat.getDrawable(context.getResources(),
-            R.drawable.icon_community, null)
-        val drWidth = dr?.intrinsicWidth
-        val drHeight = dr?.intrinsicHeight
-
-        val left = width / 2 - drWidth?.div(2) as Int
-        val top = height / 2 - drHeight?.div(2) as Int
-
-        c.drawBitmap(
-            BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_community),
-            left.toFloat(),
-            top.toFloat(),
-            null
-        )
-
-
-    }
-
-    override fun getItemOffsets(
-        outRect: Rect,
-        view: View,
-        parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
-        super.getItemOffsets(outRect, view, parent, state)
-        val index = parent.getChildAdapterPosition(view) + 1
-        if (index % 3 == 0) // left, top, right, bottom
-            outRect.set(10, 10, 10, 60)
-        else
-            outRect.set(10, 10, 10, 0)
-        view.setBackgroundColor(Color.parseColor("#28A0FF"))
-        ViewCompat.setElevation(view, 20.0f)
-    }
-}
-
 
 class CommunityFragment : Fragment() {
 
@@ -115,7 +73,6 @@ class CommunityFragment : Fragment() {
 
         val adapter = MyAdapter(datas)
         binding.communityRecyclerview.adapter = adapter
-        binding.communityRecyclerview.addItemDecoration(MyDecoration(activity as Context))
 
         // viewModel 사용법 확인을 위해 남겨둠 (지우지 마세요)
 //        val communityViewModel =
